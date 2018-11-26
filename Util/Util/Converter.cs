1de7d5a34e734fe1221520ec1288541ca0e810ed
Cleyton <cleytonnurani@gmail.com>
2018-11-26 13:31:54 -0200

    inicial

---
+++ Util/Util/Converter.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Util
+{
+    class Converter
+    {
+        public static T JsonPara<T>(string json)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static T ObjetoPara<T>(object objeto)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(objeto));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static string ObjetoParaString(object obj)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(obj);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
