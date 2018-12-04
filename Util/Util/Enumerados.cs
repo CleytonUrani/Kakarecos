2b6f826c538cd45252a1a36c8b765d67fdb2f31f
Cleyton <cleytonnurani@gmail.com>
2018-12-04 17:48:56 -0200

    Add enumerados

---
+++ Util/Util/Enumerados.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Kakarecos.Util
+{
+    public class Enumerados
+    {
+        public static string ObtemDescricao(Enum value)
+        {
+            var tipoEnum = value.GetType();
+            var campos = tipoEnum.GetField(value.ToString());
+            var atributos = campos.GetCustomAttributes(typeof(DescriptionAttribute), false);
+
+            return atributos.Length == 0 ? value.ToString() : ((DescriptionAttribute)atributos[0]).Description;
+        }
+    }
+}
