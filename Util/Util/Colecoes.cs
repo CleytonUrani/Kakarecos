4df0fe21a670d4a66b7e6b5bd6a4091383ba1f7f
Cleyton <cleytonnurani@gmail.com>
2018-12-20 12:09:16 -0200

    add colecoes

---
+++ Util/Util/Colecoes.cs
@@ -0,0 +1,19 @@
+﻿namespace Kakarecos.Util
+{
+    public class Colecoes
+    {
+        public static int[] AdicionaValorArray(int[] old, int aumento, params int[] valores)
+        {
+            int[] a = new int[old.Length + aumento];
+            int i = 0;
+
+            foreach (int num in old)
+                a.SetValue(num, i++);
+
+            foreach (int v in valores)
+                a.SetValue(v, i++);
+
+            return a;
+        }
+    }
+}
